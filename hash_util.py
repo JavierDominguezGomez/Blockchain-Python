ee70510a5da99d76f69cef693ade7612e9af9a6d
Javier Domínguez Gómez <jdomgo3@gmail.com>
2018-10-11 07:29:30 +0200

    Update blockchain.py and hash_util.py files.

--- hash_util.py
+++ hash_util.py
@@ -2,12 +2,17 @@ import hashlib as hl
 import json
 
 def hash_string_256(string):
+    """Create a SHA256 hash for a given input string.
+
+    Arguments:
+        :string: The string which should be hashed.
+    """
     return hl.sha256(string).hexdigest()
 
 
 def hash_block(block):
     """Hashes a block and returns a string representation of it.
-    
+
     Arguments:
         :block: The block that should be hashed.
     """
