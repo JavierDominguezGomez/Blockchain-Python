7d02415c59dcfa26d0af18f8e5e9a4a8044ae630
Javier Domínguez Gómez <jdomgo3@gmail.com>
2018-10-26 07:35:58 +0200

    Update wallet and node classes.

--- node.py
+++ node.py
@@ -15,6 +15,7 @@ class Node:
         # self.wallet.public_key = str(uuid4())
         self.wallet = Wallet()
         self.blockchain = None
+        self.blockchain = Blockchain(self.wallet.public_key)
 
     def get_transaction_value(self):
         """ Returns the input of the user (a new transaction amount) as a float. """
@@ -76,7 +77,8 @@ class Node:
                 self.wallet.create_keys()
                 self.blockchain = Blockchain(self.wallet.public_key)
             elif user_choice == '6':
-                pass
+                self.wallet.load_keys()
+                self.blockchain = Blockchain(self.wallet.public_key)
             elif user_choice == 'q':
                 # This will lead to the loop to exist because it's running condition becomes False
                 waiting_for_input = False
