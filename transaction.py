1699c2b692ec00cb0c8f7a2ee4e87f07a4fc2945
Javier Domínguez Gómez <jdomgo3@gmail.com>
2018-12-08 20:48:56 +0100

    PEP8 code style.

--- transaction.py
+++ transaction.py
@@ -2,6 +2,7 @@ from collections import OrderedDict
 
 from utility.printable import Printable
 
+
 class Transaction(Printable):
     """A transaction which can be added to a block in the blockchain.
 
@@ -19,4 +20,6 @@ class Transaction(Printable):
 
     def to_ordered_dict(self):
         """Converts this transaction into a (hashable) OrderedDict."""
-        return OrderedDict([('sender', self.sender), ('recipient', self.recipient), ('amount', self.amount)])
+        return OrderedDict([('sender', self.sender),
+                            ('recipient', self.recipient),
+                            ('amount', self.amount)])
