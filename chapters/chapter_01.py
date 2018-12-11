02017a450009855f501980a6f84096e119d61ec4
Javier Domínguez Gómez <jdomgo3@gmail.com>
2018-12-11 21:04:01 +0100

    Update chapter_01.py file.

--- chapters/chapter_01.py
+++ chapters/chapter_01.py
@@ -1,11 +1,18 @@
+# ESCRITURA Y LECTURA DE DATOS EN FICHEROS
 input('Please enter your name: ')
 
-my_name = input('Please enter your name: ')
+myName = input('Please enter your name: ')
+print(myName)
 
 file = open('file.txt', mode='w')
-file.write(my_name)
+file.write(myName)
 file.close()
 
 file = open('file.txt', mode='r')
 file.read()
 file.close()
+
+# LISTAS DE DATOS
+myList0 = ['Dennis', 'Ken', 'Richard']
+myList1 = [3, 5, 27]
+myList2 = ['Hola', 3.14, True, ['Otra lista!', False, 3]]
