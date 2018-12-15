19006c74532a3a4ae74c0c7f4c4fe3e14da2ca70
Javier Domínguez Gómez <jdomgo3@gmail.com>
2018-12-15 11:43:40 +0100

    Update chapter_02.py file.

--- chapters/chapter_02.py
+++ chapters/chapter_02.py
@@ -1,21 +1,26 @@
-blockchain = [[1]]
+blockchain = []
 
 
 def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_value(value):
-    blockchain.append([get_last_blockchain_value(), value])
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_amount])
 
 
-tx_amount = float(input('Your transaction amount please: '))
+def get_user_input():
+    user_input = float(input('Your transaction amount please: '))
+    return user_input
+
+
+tx_amount = get_user_input()
 add_value(tx_amount)
 
-tx_amount = float(input('Your transaction amount please: '))
-add_value(tx_amount)
+tx_amount = get_user_input()
+add_value(tx_amount, get_last_blockchain_value())
 
-tx_amount = float(input('Your transaction amount please: '))
-add_value(tx_amount)
+tx_amount = get_user_input()
+add_value(tx_amount, get_last_blockchain_value())
 
 print(blockchain)
