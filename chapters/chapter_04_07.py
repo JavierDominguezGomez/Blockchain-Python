91a1632f96be065351367f2946a1a5c02bbaff5c
Javier Domínguez Gómez <jdomgo3@gmail.com>
2018-12-29 13:18:39 +0100

    Verifying transactions.

--- chapters/chapter_04_07.py
+++ chapters/chapter_04_07.py
@@ -100,6 +100,11 @@ def verify_chain():
             return False
     return True
 
+
+def verify_transactions():
+    return all([verify_transaction(tx) for tx in open_transactions])
+
+
 waiting_for_input = True
 
 while waiting_for_input:
@@ -108,6 +113,7 @@ while waiting_for_input:
     print('2: Mine a new block')
     print('3: Output the blockchain blocks')
     print('4: Output participants')
+    print('5: Check transactions validity')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
@@ -126,6 +132,11 @@ while waiting_for_input:
         print_blockchain_elements()
     elif user_choice == '4':
         print(participants)
+    elif user_choice == '5':
+        if verify_transactions():
+            print('All transactions are valid')
+        else:
+            print('There are invalid transactions')
     elif user_choice == 'h':
         if len(blockchain) >= 1:
             blockchain[0] = {
