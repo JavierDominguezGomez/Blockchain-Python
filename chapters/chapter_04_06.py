dc5d5b9f16aeae6be88566b9b9e2ed23dc6ff635
Javier Domínguez Gómez <jdomgo3@gmail.com>
2019-01-01 12:35:27 +0100

    Fix some bugs in current_amount after process a new transaction.

--- chapters/chapter_04_06.py
+++ chapters/chapter_04_06.py
@@ -1,4 +1,4 @@
-import functools
+from functools import reduce
 
 MINING_REWARD = 10
 
@@ -20,16 +20,16 @@ def hash_block(block):
 def get_balance(participant):
     tx_sender = [[tx['amount'] for tx in block['transactions']
                  if tx['sender'] == participant] for block in blockchain]
-    amount_sent = functools.reduce(
-        lambda tx_sum, tx_amt: tx_sum + tx_amt[0]
-        if len(tx_amt) > 0 else 0,
+    amount_sent = reduce(
+        lambda tx_sum, tx_amt: tx_sum + sum(tx_amt)
+        if len(tx_amt) > 0 else tx_sum + 0,
         tx_sender, 0
     )
     tx_recipient = [[tx['amount'] for tx in block['transactions']
                     if tx['recipient'] == participant] for block in blockchain]
-    amount_received = functools.reduce(
-        lambda tx_sum, tx_amt: tx_sum + tx_amt[0]
-        if len(tx_amt) > 0 else 0,
+    amount_received = reduce(
+        lambda tx_sum, tx_amt: tx_sum + sum(tx_amt)
+        if len(tx_amt) > 0 else tx_sum + 0,
         tx_recipient, 0
     )
     return amount_received - amount_sent
